76458d176ce05cc8f15ba3549d3a79882989024d
katipogulaBhaskar <157219070+katipogulaBhaskar@users.noreply.github.com>
2024-12-11 20:58:36 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Result from './Pages/Result'
+import BuyCredit from './Pages/BuyCredit'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-slate-50'>
+      <ToastContainer position='bottom-right'/>
+      <Navbar/>
+      
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/result' element={<Result/>} />
+        <Route path='/buy' element={<BuyCredit/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
